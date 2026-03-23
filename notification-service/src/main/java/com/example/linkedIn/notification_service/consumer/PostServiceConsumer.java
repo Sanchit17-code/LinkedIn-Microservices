57303a7cd60913cf830b82417818b06011679215
Sanchit Agrawal <sanchitagrawal429@gmail.com>
2026-03-23 19:03:57 +0530

    setting up the kafka consumer and event listener in the notification for post creation and post liking

---
+++ notification-service/src/main/java/com/example/linkedIn/notification_service/consumer/PostServiceConsumer.java
@@ -0,0 +1,43 @@
+package com.example.linkedIn.notification_service.consumer;
+
+import com.example.linkedIn.notification_service.clients.ConnectionClient;
+import com.example.linkedIn.notification_service.dto.PersonDto;
+import com.example.linkedIn.notification_service.entity.Notification;
+import com.example.linkedIn.notification_service.repository.NotificationRepository;
+import com.example.linkedIn.notification_service.service.SendNotification;
+import com.example.linkedIn.posts_service.event.PostCreatedEvent;
+import com.example.linkedIn.posts_service.event.PostLikedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PostServiceConsumer {
+
+    private final ConnectionClient connectionClient;
+    private final SendNotification sendNotification;
+    @KafkaListener(topics = "post-created-topic")
+    public void handlePostCreated(PostCreatedEvent postCreatedEvent){
+        log.info("Sending notifications: handlePostCreared");
+        List<PersonDto> connections = connectionClient.getFirstConnections(postCreatedEvent.getCreatorId());
+        for(PersonDto connection: connections) {
+            sendNotification.send(connection.getUserId(), "Your connection "+postCreatedEvent.getCreatorId()+" has created" +
+                    " a post, Check it out");
+        }
+    }
+
+    @KafkaListener(topics = "post-liked-topic")
+    public void handlePostLiked(PostLikedEvent postLikedEvent) {
+        log.info("Sending notifications: handlePostLiked: {}", postLikedEvent);
+        String message = String.format("Your post, %d has been liked by %d", postLikedEvent.getPostId(),
+                postLikedEvent.getLikedByUserId());
+
+        sendNotification.send(postLikedEvent.getCreatorId(), message);
+    }
+
+}
